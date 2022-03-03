38ead9af1c1467edb47a6a5763b1a75c0024ca53
Izdhan <57706976+Prof-Iz@users.noreply.github.com>
2022-03-03 21:17:08 +0800

    changed to carbon design. Redo daisy ui better.

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,22 @@
 import adapter from '@sveltejs/adapter-auto';
 
+const production = process.env.NODE_ENV === 'production';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter(),
 
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		vite: {
+			optimizeDeps: {
+				include: ['@carbon/charts'],
+			},
+			ssr: {
+				noExternal: [production && '@carbon/charts'].filter(Boolean),
+			},
+		},
 	}
 };
 
